069523eee234baa45646b6cc6861c5821bee5563
Kevin Tsang <niveksys@gmail.com>
2020-12-31 00:50:41 +0800

    Retrieve MyController from ApplicationContext

--- src/main/java/com/niveksys/springdi/SpringDiApplication.java
+++ src/main/java/com/niveksys/springdi/SpringDiApplication.java
@@ -4,12 +4,19 @@ import com.niveksys.springdi.controller.MyController;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
--- src/main/java/com/niveksys/springdi/controller/MyController.java
+++ src/main/java/com/niveksys/springdi/controller/MyController.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Controller;
 public class MyController {
     public String sayHello() {
         System.out.println("Hello World!!!");
-        return "Hi Folks";
+        return "Hi Folks!";
     }
 }
