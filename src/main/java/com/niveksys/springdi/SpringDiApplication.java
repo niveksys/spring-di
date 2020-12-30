bc76666e63ab43fc331bb2a115c42077624068f8
Kevin Tsang <niveksys@gmail.com>
2020-12-31 00:44:56 +0800

    Create project with SPring Initializr

---
+++ src/main/java/com/niveksys/springdi/SpringDiApplication.java
@@ -0,0 +1,15 @@
+package com.niveksys.springdi;
+
+import com.niveksys.springdi.controller.MyController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiApplication.class, args);
+	}
+
+}
